075066c4d62c010c2e0d759eaf0ed6c0cac893ab
Harshvardhan Singh <harshwardhan.s@engineerbabu.in>
2020-10-01 12:10:35 +0530

    First PR Harshvardhan

---
+++ auto_mouse.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+
+i=1
+a = 0
+while i:
+    try:
+        pyautogui.moveTo(200, 100)
+        time.sleep(100)
+        pyautogui.moveTo(100, 600)
+        time.sleep(100)
+        pyautogui.moveTo(600, 900)
+        time.sleep(100)
+
+        a += 1
+        print("Iterations = ",a)
+    except:
+        print('Exception')
+
+
+# In[22]:
+
+
+pyautogui.size()
+
+
+# In[23]:
+
+
+pyautogui.moveTo(10, 10)
